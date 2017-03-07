71d54f6aa7b33ca724379c8f0048c99414075196
Chance Turner <chance.turner@eagles.oc.edu>
2017-03-07 14:11:28 -0600

    Have a working background thread that will send notifications every so many minutes

--- PPOk Notification System/PPOk Notifications/NotificationSending/NotificationSender.cs
+++ PPOk Notification System/PPOk Notifications/NotificationSending/NotificationSender.cs
@@ -2,22 +2,94 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Threading;
-using WebBackgrounder;
-
-[assembly: WebActivator.PreApplicationStartMethod(
-  typeof(NotificationSending), "Start")]
-
+using System.Web.Hosting;
+using System.IO;
 
 namespace PPOk_Notifications.NotificationSending
 {
-    public class NotificationSender
+    public class NotificationSender : IRegisteredObject
     {
+        string path = HttpContext.Current.Server.MapPath("~/App_Data/lastNoficiationSent.bin");
+        int minsBetweenSending = 1;
 
+        private readonly object _lock = new object();
+        private bool _shuttingDown;
+
+        public NotificationSender()
+        {
+            HostingEnvironment.RegisterObject(this);
+        }
+
+        public void Stop(bool immediate)
+        {
+            lock (_lock)
+            {
+                _shuttingDown = true;
+            }
+            HostingEnvironment.UnregisterObject(this);
+        }
+
+        public void DoWork()
+        {
+            lock (_lock)
+            {
+                if (_shuttingDown)
+                {
+                    return;
+                }
+                sendNotifications();
+            }
+        }
 
         private void sendNotifications()
         {
+            DateTime? lastDTwhenSent = null;
+            if (File.Exists(path)) {
+                lastDTwhenSent = readDateFromFile();
+            }
+            if (lastDTwhenSent != null)
+            {
+                TimeSpan span = DateTime.Now.Subtract((DateTime)lastDTwhenSent);
+                if (span.Minutes >= minsBetweenSending)
+                {
+                    writeDateToFile();
+                    System.Diagnostics.Debug.WriteLine("Sending");
+                }
+            }
 
+        }
+
+        private Boolean writeDateToFile()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter writer = new BinaryWriter(fs))
+                {
+                    fs.Position = 0;
+                    writer.Write(DateTime.Now.ToString());
+                    writer.Close();
+                }
+                fs.Close();
+            }
+            return true;
+        }
+
+        private DateTime? readDateFromFile()
+        {
+            DateTime? parsedDateTime = null;
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    fs.Position = 0;
+                    if (fs.Position < fs.Length)
+                    {
+                        string dtString = reader.ReadString();
+                        parsedDateTime = DateTime.Parse(dtString);                    }
+                }
+                fs.Close();
+            }
+            return parsedDateTime;
         }
     }
 }
