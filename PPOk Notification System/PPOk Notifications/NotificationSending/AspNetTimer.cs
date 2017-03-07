0ae5e35f44ecc32896b8543354a58862885d16f0
Chance Turner <chance.turner@eagles.oc.edu>
2017-03-07 13:06:44 -0600

    Started notification sending

---
+++ PPOk Notification System/PPOk Notifications/NotificationSending/AspNetTimer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPOk_Notifications.NotificationSending
+{
+	public class AspNetTimer
+	{
+	}
+}
---
+++ PPOk Notification System/PPOk Notifications/NotificationSending/NotificationSender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using WebBackgrounder;
+
+[assembly: WebActivator.PreApplicationStartMethod(
+  typeof(NotificationSending), "Start")]
+
+
+namespace PPOk_Notifications.NotificationSending
+{
+    public class NotificationSender
+    {
+
+
+        private void sendNotifications()
+        {
+
+        }
+    }
+}
