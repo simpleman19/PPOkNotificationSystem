865b866dabefe3a93c2b5260feba69834f1b396a
Chance Turner <chance.turner@eagles.oc.edu>
2017-03-08 12:37:49 -0600

    Added template and pharmacy classes

---
+++ PPOk Notification System/PPOk Notifications/Models/Pharmacy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPOk_Notifications.Models
+{
+    public class Pharmacy
+    {
+        public long pharmacyID { get; set; }
+        public String pharmacyName { get; set; }
+        public String pharmacyPhone { get; set; }
+        public String pharmacyAddress { get; set; }
+        public Template templateRefill { get; set; }
+        public Template templateReady { get; set; }
+        public Template templateRecall { get; set; }
+        public Template templateBirthday { get; set; }
+
+    }
+}
--- PPOk Notification System/PPOk Notifications/Models/Template.cs
+++ PPOk Notification System/PPOk Notifications/Models/Template.cs
@@ -9,6 +9,9 @@ namespace PPOk_Notifications.Models
     {
         public long templateID { get; set; }
         public long pharmacyID { get; set; }
-        
+        public String templateEmail { get; set; }
+        public String templateText { get; set; }
+        public String templatePhone { get; set; }
+
     }
 }
