d0c1a3f56e7e5092ee222c923928cdad0f31cd82
Tyler Kerr <destarianon@gmail.com>
2017-03-19 23:43:10 -0500

    Added User Methods to SQLService Class Created regions for future SQL methods

--- PPOk Notification System/PPOk Notifications/Service/SQLService.cs
+++ PPOk Notification System/PPOk Notifications/Service/SQLService.cs
@@ -5,6 +5,7 @@ using Dapper;
 using Dapper.Contrib.Extensions;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
 
 namespace PPOk_Notifications.Service {
 	public class SQLService {
@@ -59,6 +60,203 @@ namespace PPOk_Notifications.Service {
 		 */
 		#region User Operations
 
+		#region Enable/Disable Operations
+		public void User_Enable(int user_id) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_enable"], user_id);
+			}
+		}
+		public void User_Disable(int user_id) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_disable"], new {user_id = user_id} );
+			}
+		}
 		#endregion
+
+		#region Get all
+		public List<User> GetUsers() {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getall"]).AsList();
+			}
+		}
+		public List<User> GetUsersActive() {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getall_active"]).AsList();
+			}
+		}
+		public List<User> GetUsersInactive() {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getall_inactive"]).AsList();
+			}
+		}
+		#endregion
+
+		#region Get by id
+		public User GetUserById(int user_id) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyid"], new { user_id = user_id }).FirstOrDefault();
+			}
+		}
+		public User GetUserByIdActive(int user_id) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyid_active"], new { user_id = user_id }).FirstOrDefault();
+			}
+		}
+		public User GetUserByIdInactive(int user_id) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyid_inactive"], new { user_id = user_id }).FirstOrDefault();
+			}
+		}
+		#endregion
+
+		#region Get by email
+		public User GetUserByEmail(string user_email) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyemail"], new { user_email = user_email }).FirstOrDefault();
+			}
+		}
+		public User GetUserByEmailActive(string user_email) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyemail_active"], new { user_email = user_email }).FirstOrDefault();
+			}
+		}
+		public User GetUserByEmailInactive(string user_email) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyemail_inactive"], new { user_email = user_email }).FirstOrDefault();
+			}
+		}
+		#endregion
+
+		#region Get by name
+		public User GetUserByName(string user_fname, string user_lname) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyname"], new { user_fname = user_fname, user_lname = user_lname }).FirstOrDefault();
+			}
+		}
+		public User GetUserByNameActive(string user_fname, string user_lname) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyname_active"], new { user_fname = user_fname, user_lname = user_lname }).FirstOrDefault();
+			}
+		}
+		public User GetUserByNameInactive(string user_fname, string user_lname) {
+			using (var db = connect()) {
+				return db.Query<User>(ScriptService.Scripts["user_getbyname_inactive"], new { user_fname = user_fname, user_lname = user_lname }).FirstOrDefault();
+			}
+		}
+		#endregion
+
+		#region Insert
+		public void UserInsert(User user) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_insert"], user);
+			}
+		}
+		public void UserInsertOrUpdate(User user) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_insert_or_update"], user);
+			}
+		}
+		#endregion
+
+		#region Update
+		public void UserUpdate(User user) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_update"], user);
+			}
+		}
+		public void UserUpdateActive(User user) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_update_active"], user);
+			}
+		}
+		public void UserUpdateInactive(User user) {
+			using (var db = connect()) {
+				db.Execute(ScriptService.Scripts["user_update_inactive"], user);
+			}
+		}
+		#endregion
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Login Operations
+		 */
+		#region Login Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Pharmacy Operations
+		 */
+		#region Pharmacy Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Pharmacist Operations
+		 */
+		#region Pharmacist Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Template Operations
+		 */
+		#region Template Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Patient Operations
+		 */
+		#region Patient Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Prescription Operations
+		 */
+		#region Prescription Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Refill Operations
+		 */
+		#region Refill Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     Notification Operations
+		 */
+		#region Notification Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     OTP Operations
+		 */
+		#region OTP Operations
+
+		#endregion
+
+		/*
+		 * =======================================
+		 *     EmailOTP Operations
+		 */
+		#region EmailOTP Operations
+
+		#endregion
+
 	}
 }
