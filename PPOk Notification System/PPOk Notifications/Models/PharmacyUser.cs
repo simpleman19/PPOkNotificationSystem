6e04d3b688db954bcc8d4b238207b8428ac97733
DemonWav <demonwav@gmail.com>
2017-03-20 14:13:31 -0500

    Fix typos and other stuff I can't remember

--- PPOk Notification System/PPOk Notifications/Models/PharmacyUser.cs
+++ PPOk Notification System/PPOk Notifications/Models/PharmacyUser.cs
@@ -8,16 +8,16 @@ namespace PPOk_Notifications.Models
         public long PharmacyId { get; set; }
         public bool IsAdmin { get; set; }
         public byte[] PasswordHash { get; set; }
-        public string Salt { get; set; }
+        public byte[] Salt { get; set; }
 
         public void GenerateSalt()
         {
-            var salt = new byte[64];
+            var salt = new byte[32];
             using (var random = new RNGCryptoServiceProvider())
             {
                 random.GetNonZeroBytes(salt);
             }
-            Salt = Convert.ToBase64String(salt);
+            Salt = salt;
         }
     }
 }
