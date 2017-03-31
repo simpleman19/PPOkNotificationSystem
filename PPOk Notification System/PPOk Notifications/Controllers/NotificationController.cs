12dd8df0167040ad4a4d13f161b0b0a5bb1940dd
Chance Turner <chance.turner@eagles.oc.edu>
2017-03-30 22:46:56 -0500

    Working on login and test login stuff

--- PPOk Notification System/PPOk Notifications/Controllers/NotificationController.cs
+++ PPOk Notification System/PPOk Notifications/Controllers/NotificationController.cs
@@ -15,60 +15,6 @@ namespace PPOk_Notifications.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult NotificationListView()
-        {
-            IEnumerable<PPOk_Notifications.Models.Notification> param = new List<PPOk_Notifications.Models.Notification>();
-            PPOk_Notifications.Service.SQLService serv = new PPOk_Notifications.Service.SQLService();
-            ((List<PPOk_Notifications.Models.Notification>)param).AddRange(serv.GetNotifications());
-            // return view
-
-            if (Request.IsAjaxRequest())
-            {
-                return PartialView("NotificationListView", new System.Tuple<IEnumerable<PPOk_Notifications.Models.Notification>,
-                    PPOk_Notifications.Service.SQLService>
-                    (param, serv));
-            }
-            else
-            {
-                return View(new System.Tuple<IEnumerable<PPOk_Notifications.Models.Notification>,
-                    PPOk_Notifications.Service.SQLService>
-                    (param, serv));
-            }
-        }
-        [HttpGet]
-        public ActionResult NotificationListView(string searchString)
-        {
-            PPOk_Notifications.Service.SQLService serv = new PPOk_Notifications.Service.SQLService();
-            List<PPOk_Notifications.Models.Notification> param = new List<PPOk_Notifications.Models.Notification>();
-            List<PPOk_Notifications.Models.Notification> filtered = new List<PPOk_Notifications.Models.Notification>();
-            param.AddRange(serv.GetNotifications());
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                foreach (var item in param)
-                {
-                    if (item.NotificationId.ToString().Contains(searchString) ||
-                        item.Type.ToString().Contains(searchString) ||
-                        item.PatientId.ToString().Contains(searchString) ||
-                        serv.GetPatientById((int)item.PatientId).FirstName.Contains(searchString) ||
-                        serv.GetPatientById((int)item.PatientId).LastName.Contains(searchString))
-                    {
-                        filtered.Add(item);
-                    }
-                }
-            }
-            else { filtered = param; }
-
-            if (Request.IsAjaxRequest())
-            {
-                return PartialView("NotificationListView", filtered);
-            }
-            else
-            {
-                return View(filtered);
-            }
-        }
-
         public ActionResult CancelNotification(long id)
         {
             var db = new SQLService();
@@ -90,16 +36,12 @@ namespace PPOk_Notifications.Controllers
             }
         }
 
-
-        /*
         public ActionResult NotificationList()
         {
-            List<Notification> list = new List<Notification>(); 
-            for (int i = 0; i < 100; i++)
-            {
-                list.Add(Notification.GetTestNotification());
-            }
-            return View(list);
+            var db = new SQLService();
+            List<Notification> notifications = db.GetNotificationsActive();
+
+            return View(notifications);
         }
 
         public ActionResult DeleteNotification(long id)
@@ -108,6 +50,5 @@ namespace PPOk_Notifications.Controllers
             db.Notification_Disable(id);
             return Redirect("/Notification/NotificationList");
         }
-        */
     }
 }
