a3fb4f130d5eb94d51a7ebd50593544f9da6e939
Chance Turner <chance.turner@eagles.oc.edu>
2017-04-02 17:01:33 -0500

    Fixed route config

--- PPOk Notification System/PPOk Notifications/App_Start/RouteConfig.cs
+++ PPOk Notification System/PPOk Notifications/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace PPOk_Notifications
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "PharmacistLogin", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
