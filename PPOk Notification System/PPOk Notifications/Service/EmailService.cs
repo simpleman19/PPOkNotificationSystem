f624de4f42815bd2f770d4d239970ea090e52f20
Tyler Kerr <destarianon@gmail.com>
2017-04-02 18:27:49 -0500

    Added EmailSerrvice with working test email account. Added EmailService setting subject line automatically.

--- PPOk Notification System/PPOk Notifications/Service/EmailService.cs
+++ PPOk Notification System/PPOk Notifications/Service/EmailService.cs
@@ -6,51 +6,55 @@ using System.Net.Mail;
 using System.Web;
 using PPOk_Notifications.Models;
 
-namespace PPOk_Notifications.Service
-{
-    public class EmailService
-    {
-        public string Contact(Notification notification)
-        {
-            SQLService db = new SQLService();
+namespace PPOk_Notifications.Service {
+	public class EmailService {
+		public static bool Send(Notification notification) {
+			SQLService db = new SQLService();
 
-            Patient patient = db.GetPatientById(notification.PatientId);
-            User user = db.GetUserById(patient.UserId);
+			Patient patient = db.GetPatientById(notification.PatientId);
+			User user = db.GetUserById(patient.UserId);
+			Pharmacy pharmacy = db.GetPharmacyById(patient.PharmacyId);
 
-            var body = "<p>You recieved a badge from {0} ({1})</p>" +
-                "<p>You recived: {2}<br/>" +
-                "Reson: {3}<br/>" +
-                "<img src={4}></p>" +
-                "<p>{0} wanted to tell you \"{5}\"</p>";
-            var message = new MailMessage();
-            message.To.Add(new MailAddress(user.Email));
-            message.From = new MailAddress("");
-            message.Subject = "Test Email";
-            //message.Body = string.Format(body, senduser.user_name, senduser.user_email, badge.badge_name, badge.badge_description, badge.badge_gifURL, awardedBadge.award_comment);
-            message.IsBodyHtml = true;
+			var message = new MailMessage();
+			message.To.Add(new MailAddress(user.Email));
+			message.From = new MailAddress("ppoknotifier@gmail.com");
 
-            try
-            {
-                using (var smtp = new SmtpClient())
-                {
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "bibletreeproject@gmail.com", //TESTING ACCOUNT
-                        Password = "kMbCiz2GsNsU"
-                    };
-                    smtp.Credentials = credential;
-                    smtp.Host = "smtp.gmail.com";
-                    smtp.Port = 587;
-                    smtp.EnableSsl = true;
+			var body = "";
 
-                    smtp.Send(message);
-                }
-            }
-            catch (Exception e)
-            {
-                return "<span style='color:red'>fail</span> <br/>[reason:] " + e.Message;
-            }
-            return "<span style='color:green'>success</span>";
-        }
-    }
+			if (notification.Type == Notification.NotificationType.Birthday) {
+				message.Subject = "Happy Birthday, from " + pharmacy.PharmacyName + "!";
+			} else if (notification.Type == Notification.NotificationType.Ready) {
+				message.Subject = "Your Refill is ready to be picked up!";
+			} else if (notification.Type == Notification.NotificationType.Recall) {
+				message.Subject = "Important! A Prescription has been recalled!";
+				message.Priority = MailPriority.High;
+			} else if (notification.Type == Notification.NotificationType.Refill) {
+				message.Subject = "Would you like to refill your prescription?";
+			} else {
+				message.Subject = "Test Email";
+			}
+
+			//message.Body = string.Format(body, senduser.user_name);
+			message.IsBodyHtml = true;
+
+			try {
+				using (var smtp = new SmtpClient()) {
+					var credential = new NetworkCredential {
+						UserName = "ppoknotifier@gmail.com", //TESTING ACCOUNT
+						Password = "L7BQ2aOYpEO9lXiAVW9uEA2Rju9X7scFLN7M7RUhpuRQ9fAcllk4be0bThlQfj1eqgj4YzII6Ve10gBbKrmtv5T9JYlRWmHq5s9w"
+					};
+					smtp.Credentials = credential;
+					smtp.Host = "smtp.gmail.com";
+					smtp.Port = 587;
+					smtp.EnableSsl = true;
+
+					smtp.Send(message);
+				}
+			}
+			catch (Exception e) {
+				return false;
+			}
+			return true;
+		}
+	}
 }
