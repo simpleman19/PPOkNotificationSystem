04ce6120f6a1be561f411ea05b46eccc4b86e7f6
conflictensues <gage.william.ervin@gmail.com>
2017-04-13 23:19:24 -0500

    Removed loaduserdata from pharmacist.cs which was added by chance to user.cs

--- PPOk Notification System/PPOk Notifications/Models/Pharmacist.cs
+++ PPOk Notification System/PPOk Notifications/Models/Pharmacist.cs
@@ -16,15 +16,6 @@ namespace PPOk_Notifications.Models {
         {
             this.Type = UserType.Pharmacist;
         }
-
-        public Pharmacist LoadUserData()
-        {
-            var user = DatabaseUserService.GetById(UserId);
-            FirstName = user.FirstName;
-            LastName = user.LastName;
-            Email = user.Email;
-            Phone = user.Phone;
-            return this;
-        }
+        
     }
 }
