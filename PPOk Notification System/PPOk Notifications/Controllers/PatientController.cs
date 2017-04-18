72704483023da31b6f9b4a62c53a57527ce5e543
DemonWav <demonwav@gmail.com>
2017-04-18 14:21:38 -0500

    Add basic work for the patient preferences page and patient logins

---
+++ PPOk Notification System/PPOk Notifications/Controllers/PatientController.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using PPOk_Notifications.Service;
+
+namespace PPOk_Notifications.Controllers
+{
+    public class PatientController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(DatabasePatientService.GetByPersonCode("1", 1));
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string phonenumber)
+        {
+            // just a bit of input cleanup
+            phonenumber = new Regex("[\\(\\)\\s+\\-]").Replace(phonenumber, "");
+            if (!phonenumber.StartsWith("+"))
+            {
+                if (phonenumber.Length == 10)
+                {
+                    phonenumber = "+1" + phonenumber;
+                }
+                else
+                {
+                    phonenumber = "+" + phonenumber;
+                }
+            }
+            else
+            {
+                if (phonenumber.Length == 11)
+                {
+                    phonenumber = "+1" + phonenumber.Substring(1);
+                }
+            }
+
+            // TODO Tyler - skip this step and get patient directly from phone number?
+            var user = DatabaseUserService.GetByPhoneActive(phonenumber);
+            if (user == null)
+            {
+                return RedirectToAction("Code"); // just act like there's no problem
+            }
+
+            var patient = DatabasePatientService.GetByUserIdActive(user.UserId);
+            if (patient == null)
+            {
+                return RedirectToAction("Code"); // just act like there's no problem
+            }
+
+            // TODO send code, write it to DB, redirect to Code page
+
+            return RedirectToAction("Code");
+        }
+
+        public ActionResult Code()
+        {
+            return View();
+        }
+    }
+}
--- PPOk Notification System/PPOk Notifications/Service/TwilioApi.cs
+++ PPOk Notification System/PPOk Notifications/Service/TwilioApi.cs
@@ -9,10 +9,12 @@ namespace PPOk_Notifications.Service
 {
     public class TwilioApi
     {
-        private const string AccountSid = "AC2fcaf6f7256a0f7891903318195c9e01";
-        private const string AuthToken = "7504188a03f5969ff3549eaf8fba3e9c";
+//        private const string AccountSid = "AC2fcaf6f7256a0f7891903318195c9e01";
+        private const string AccountSid = "AC959383919ad257c0c8c69bb8b358ff43";
+//        private const string AuthToken = "7504188a03f5969ff3549eaf8fba3e9c";
+        private const string AuthToken = "85231a1a333ad4fbbc61eaed854e492b";
 
-        Pharmacy pharmacy;
+        readonly Pharmacy pharmacy;
 
         bool testTwilio = true;
 
@@ -34,7 +36,6 @@ namespace PPOk_Notifications.Service
                     from: new PhoneNumber("+14052469892 "),
                     body: temp.TemplateText);
             }
-
         }
 
         public void SendTextMessage(Patient p, String messageString)
