193b667d011d119428dd7203e0fea25d45570622
Tyler Kerr <destarianon@gmail.com>
2017-04-19 00:35:28 -0500

    Added get refills with prescriptions method. (Incomplete)

---
+++ PPOk Notification System/PPOk Notifications/Service/SQL/Prescription_GetAllWithRefills.sql
@@ -0,0 +1,6 @@
+﻿SELECT r.*, pers.*
+FROM [PPOK].[dbo].[refill] r
+INNER JOIN [PPOK].[dbo].[prescription] pers ON r.[prescription_id] = pers.[prescription_id]
+INNER JOIN [PPOK].[dbo].[patient] pat ON pat.[patient_id] = pers.[patient_id]
+INNER JOIN [PPOK].[dbo].[pharmacy] pharm ON pat.[pharmacy_id] = pharm.[pharmacy_id]
+WHERE r.[object_active] = 1 AND pharm.[pharmacy_id] = @PharmacyId
